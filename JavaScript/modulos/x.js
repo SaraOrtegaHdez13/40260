c1ce930b77a72ff8fe179b13674fdd3a3abd5be6
SaraOrtegaHdez13 <SaraOrtegaHdez13@gmail,com>
2026-05-18 12:12:08 -0600

    js con y sin modulos

---
+++ JavaScript/modulos/x.js
@@ -0,0 +1,14 @@
+//forzosamente se tiene que importar "y"
+import {y} from "./y.js"
+
+function x(param){
+    y("x")
+    console.log("x:"+param)
+    return "fin x"
+}
+
+function xx(param){
+    return "fin xx"
+}
+
+export {x, xx}
