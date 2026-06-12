94babc5f174115bce52a98098127d34ed8e9397f
SaraOrtegaHdez13 <SaraOrtegaHdez13@gmail,com>
2026-06-11 21:43:23 -0600

    Ejercicios de xml, javascript, html y css

--- JavaScript/javascript-02.js
+++ JavaScript/javascript-02.js
@@ -9,106 +9,168 @@
 // let b9 = document.getElementById("b9")
 // let b0 = document.getElementById("b0")
 
-// let resultado = document.getElementById("resultado")
-// let botones = document.getElementsByTagName("button")
+let resultado = document.getElementById("resultado")
+let botones = document.getElementsByTagName("button")
+for (const key in botones) {
+    if (Object.prototype.hasOwnProperty.call(botones, key)){
+        const boton = botones[key];
+        if (boton.className != "operadores")
+            boton.addEventListener("click", pintar)
+        else
+            boton.addEventListener("click", operadores)
+        //console.log(boton)
+
+    }
+}
+
+function pintar(e){
+    console.log(e.target.innerText)
+    resultado.value += e.target.innerText
+    eventos.push(e.target.innerText)
+}
+
+// let operadores = document.getElementsByClassName("operadores")
+//     for (const key in operadores) {
+//         if (Object.prototype.hasOwnProperty.call(operadores, key)){
+//             const boton = operadores[key];
+//             boton.addEventListener("click", pintar2)
+//             //console.log(boton)
+
+//         }
+//     }
+
+function operadores(e){
+    let operador = e.target.innerText
+    eventos.push()
+    if (operador == "=")
+        igual()
+    else
+        almacena_parametro(operador)
+}
+
+let prm1
+let operacion
+function almacena_parametro(prm){
+    //prm1 = resultado.value
+    parametros.push(resultado.value)
+
+    eventos.push(resultado.value)
+    resultado.value = ""
+    operacion = prm
+    eventos.push(operacion)
+
+}
+
+
+function igual(){
+    //prm2 = resultado.value
+    let variable = 0
+    parametros.push(resultado.value)
+    eventos.push(resultado.value)
+    for (const key in parametros) {
+        if (Object.prototype.hasOwnProperty.call(parametros, key)) {
+
+                    
+            const prm = parametros[key];
+
+            if (operacion == "+")
+                variable += parseInt(prm)
+
+
+            if (operacion == "-")
+                if (bandera == false){
+                    variable =  parseInt(prm)
+                    bandera = true
+   
+                }else
+                    variable=  parseInt(prm)
+
+
+        
+        }
+
+
+
+        
+    }
+
+    resultado.value=variable
+    // if (operacion == "+")
+    //     resultado.value = parseInt(prm1) + parseInt(prm2)
+    // if (operacion == "-")
+    //     resultado.value = parseInt(prm1) - parseInt(prm2)
+    // if (operacion == "*")
+    //     resultado.value = parseInt(prm1) * parseInt(prm2)
+    // if (operacion == "/")
+    //     resultado.value = parseInt(prm1) / parseInt(prm2)
+}
+
+let parametros = []
+let eventos = []
+
+//w3schools let consy javbacript
+//Dentro de ahí ponemos hoisted
+
+
+
+
+
+// let resultado = document.getElementById("resultado");
+// let botones = document.getElementsByTagName("button");
+
+// // Variables para almacenar los valores y la operación
+// let prm1 = 0;
+// let operacion = "";
+
 // for (const key in botones) {
-//     if (Object.prototype.hasOwnProperty.call(botones, key)){
+//     if (Object.prototype.hasOwnProperty.call(botones, key)) {
 //         const boton = botones[key];
 //         if (boton.className != "operadores")
-//             boton.addEventListener("click", pintar)
+//             boton.addEventListener("click", pintar);
 //         else
-//             boton.addEventListener("click", pintar2)
-//         //console.log(boton)
-
+//             boton.addEventListener("click", pintar2);
 //     }
 // }
 
-// function pintar(e){
-//     console.log(e.target.innerText)
-//     resultado.value += e.target.innerText
+// function pintar(e) {
+//     resultado.value += e.target.innerText;
 // }
 
-// // let operadores = document.getElementsByClassName("operadores")
-// //     for (const key in operadores) {
-// //         if (Object.prototype.hasOwnProperty.call(operadores, key)){
-// //             const boton = operadores[key];
-// //             boton.addEventListener("click", pintar2)
-// //             //console.log(boton)
-
-// //         }
-// //     }
-
-// function pintar2(e){
-//     console.log(e.target.innerText)
-//     if (e.target.innerText == "+")
-//         suma()
-// }
-
-// let prm1
-// function suma(){
-//     prm1 = resultado.value
-//     resultado.value=""
-
-// }
-
-// function igual(){
-//     prm2 = resultado.value
-//     resultado.value = parseInt(prm1) + parseInt(prm2)
-// }
-
-
-let resultado = document.getElementById("resultado");
-let botones = document.getElementsByTagName("button");
-
-// Variables para almacenar los valores y la operación
-let prm1 = 0;
-let operacion = "";
-
-for (const key in botones) {
-    if (Object.prototype.hasOwnProperty.call(botones, key)) {
-        const boton = botones[key];
-        if (boton.className != "operadores")
-            boton.addEventListener("click", pintar);
-        else
-            boton.addEventListener("click", pintar2);
-    }
-}
-
-function pintar(e) {
-    resultado.value += e.target.innerText;
-}
-
-function pintar2(e) {
-    let signo = e.target.innerText;
+// function pintar2(e) {
+//     let signo = e.target.innerText;
     
-    if (signo === "=") {
-        igual();
-    } else if (signo === "Ce") {
-        limpiar();
-    } else {
-        // Guardamos el primer número y el tipo de operación
-        prm1 = resultado.value;
-        operacion = signo;
-        resultado.value = "";
-    }
-}
+//     if (signo === "=") {
+//         igual();
+//     } else if (signo === "Ce") {
+//         limpiar();
+//     } else {
+//         // Guardamos el primer número y el tipo de operación
+//         prm1 = resultado.value;
+//         operacion = signo;
+//         resultado.value = "";
+//     }
+// }
 
-function igual() {
-    let prm2 = resultado.value;
-    let calculo = 0;
-    let n1 = parseFloat(prm1);
-    let n2 = parseFloat(prm2);
+// function igual() {
+//     let prm2 = resultado.value;
+//     let calculo = 0;
+//     let n1 = parseFloat(prm1);
+//     let n2 = parseFloat(prm2);
 
-    if (operacion === "+") calculo = n1 + n2;
-    if (operacion === "-") calculo = n1 - n2;
-    if (operacion === "*") calculo = n1 * n2;
-    if (operacion === "/") calculo = n1 / n2;
+//     if (operacion === "+") calculo = n1 + n2;
+//     if (operacion === "-") calculo = n1 - n2;
+//     if (operacion === "*") calculo = n1 * n2;
+//     if (operacion === "/") calculo = n1 / n2;
 
-    resultado.value = calculo;
-}
+//     resultado.value = calculo;
+// }
 
-function limpiar() {
-    resultado.value = "";
-    prm1 = 0;
-    operacion = "";
-}
+// function limpiar() {
+//     resultado.value = "";
+//     prm1 = 0;
+//     operacion = "";
+// }
+
+// Buscar el codigo en el repositorio del profe
+
+// Como en base del array sacando sus elementos podemos hacer las operaciones
---
+++ JavaScript/xyz.js
@@ -0,0 +1,6 @@
+console.log(x("abc"))
+console.log(x("def"))
+console.log(x("ghi"))
+console.log(x("jkl"))
+
+
